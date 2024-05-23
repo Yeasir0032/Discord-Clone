bfc8bb2e24b6383633e5d6cc2e08a0af2046ec54
Latish 705 <104522651+Latish705@users.noreply.github.com>
2024-05-23 16:14:43 +0530

    #18 Creating a explore page for servers (#33)
    adding new page for exploring servers

---
+++ app/api/servers/getAllServers/route.ts
@@ -0,0 +1,17 @@
+import { supabase } from "@/components/client";
+import { Request, Response } from "express";
+
+export async function GET(req: Request, res: Response) {
+  try {
+    const { data: servers, error } = await supabase.from("Server").select();
+    if (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      res.status(200).json(servers);
+    }
+  } catch (error: any) {
+    console.error("Error fetching servers:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
